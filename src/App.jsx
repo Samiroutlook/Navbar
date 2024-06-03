f43814f8f2013dc123bd6bb2169b64a15c0fbbf1
Samiroutlook <samirmc8384@outlook.com>
2024-06-03 14:10:15 +0600

    add routs

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import Nav from "./Components/Navbar/Nav";
 import theme from "./theme";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
+import Projects from "./Components/Pages/Projects";
+import Contact from "./Components/Pages/Contact";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </ChakraProvider>
   );
 }
