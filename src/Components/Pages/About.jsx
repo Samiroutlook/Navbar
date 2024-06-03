ef1b94c17535146ad1aeaae08b8223a2c257da28
Samiroutlook <samirmc8384@outlook.com>
2024-06-03 17:25:40 +0600

    add routs

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function About() {
-  return <div>Hello Projects</div>;
+  return <div>About</div>;
 }
 
 export default About;
--- src/Components/Pages/Projects.jsx
+++ src/Components/Pages/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Projects() {
-  return <div>Hello About</div>;
+  return <div>Projects</div>;
 }
 
 export default Projects;
