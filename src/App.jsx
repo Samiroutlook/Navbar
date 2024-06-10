054fa39b1c4d6d145d679e7a077cf51d16972c93
Samiroutlook <samirmc8384@outlook.com>
2024-06-10 19:04:18 +0600

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="Navbar/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
