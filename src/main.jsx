1888fc4b792fd4c07f52aaa1a8cddabbece69920
Samiroutlook <samirmc8384@outlook.com>
2024-06-11 18:04:36 +0600

    add all

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL || "/"}>
+    <BrowserRouter basename="/Navbar">
       <ChakraProvider>
         <App />
       </ChakraProvider>
